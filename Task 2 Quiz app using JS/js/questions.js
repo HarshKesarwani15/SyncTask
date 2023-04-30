33c339877a31c2dcabf2df6904e22aa215392a16
Harsh Kesarwani <93967185+HarshKesarwani15@users.noreply.github.com>
2023-04-30 18:40:09 +0530

    Add files via upload

---
+++ Task 2 Quiz app using JS/js/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of the following is linear data structure?",
+    answer: "Arrays",
+    options: [
+      "Trees",
+      "Graphs",
+      "Arrays",
+      "None of these"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Queue is based on which principle? ",
+    answer: "FIFO",
+    options: [
+      "FIFO",
+      "LIFO",
+      "pop",
+      "push"
+    ]
+  },
+    {
+    numb: 4,
+    question: "unction is to specify which data type?",
+    answer: "typedef",
+    options: [
+      "arr",
+      "typedef",
+      "id",
+      "struct"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+];
